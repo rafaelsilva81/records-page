313303b986e2ed752db3028faa3a2d35c7084442
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2021-09-09 13:19:52 -0300

    FIX : Security Fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { faBars, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
-import { faInstagram, faFacebookF, faTwitch, faSpotify } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faFacebookF, faTwitch, faDiscord } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image';
 
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="description" content="Página Web do podcast mais BANGOS do Brasil!" />
       </Head>
 
-      <div className="header">
+      <div id="header">
         <nav className="py-2 bg-gray-900 md:py-4">
           <div className="container px-4 mx-auto md:px-6 md:flex md:items-center">
 
@@ -43,8 +43,8 @@ export default function Home() {
               <a href="#" className="font-bold text-white link-button md:px-0 hover:text-purple-600">
                 <FontAwesomeIcon icon={faTwitch} size="lg"/> 
               </a>
-              <a href="#" className="font-bold text-white link-button md:px-0 hover:text-green-500">
-                <FontAwesomeIcon icon={faSpotify} size="lg"/> 
+              <a href="#" className="font-bold text-white link-button md:px-0 hover:text-blue-400">
+                <FontAwesomeIcon icon={faDiscord} size="lg"/> 
               </a>
             </div>
             
@@ -52,8 +52,43 @@ export default function Home() {
         </nav>
       </div>
 
-      <div className="h-screen px-4 mx-auto bg-gray-900 main md:px-6"> 
-        <div className="flex flex-col items-center justify-center md:py-6 md:px-10 bg-secondary md:justify-between md:flex-row md:rounded-2xl">
+      <div className="h-screen px-4 mx-auto bg-gray-900 md:px-6" id="main-content"> {/* DIV PRINCIPAL */} 
+
+          <div className="flex flex-col container bg-secondary rounded-md"> {/* SEÇÃO INICIAL */}
+
+            <div className="flex flex-col px-6 pt-6 justify-centertext-center md:text-left "> {/* Texto da seção inicial */}
+              <h1 className="text-4xl mb-2 font-extrabold md:text-7xl text-white"> PODBANGOS </h1>
+              <p className="text-white md:text-2xl"> 
+                Os famosos especialistas em p#rra nenhuma, sem roteiro, apenas amigos em trocando uma ideia exacerbada 
+              </p>
+            </div>
+
+            <div className="flex flex-row justify-center md:justify-start md:ml-6 pb-4 mt-4"> {/* Botões call-to-action */}
+              <button className="btn mr-2"> Conheça Melhor </button>
+              <button className="btn-outline"> 
+                Ver Episódios
+              </button>
+            </div>
+            
+            <div className="md:hidden px-5 pb-5"> {/* Imagem no podcast no mobile */}
+              <Image
+                src="/logo1.png"
+                alt="Imagem do podcast"
+                width={500}
+                height={500}
+                layout="responsive"
+              />
+            </div>
+
+          </div>
+
+      </div>
+    </>
+  )
+}
+
+
+       {/*  <div className="flex flex-col items-center justify-center md:py-6 md:px-10 bg-secondary md:justify-between md:flex-row md:rounded-2xl">
           <div className="flex flex-col mr-14 items-left">
             <h1 className="text-4xl font-extrabold text-white md:my-3 lg:text-8xl md:text-7xl"> PODBANGOS </h1>
             <p className="text-white md:text-2xl"> Os famosos especialistas em porra nem uma, sem roteiro, apenas amigos trocando uma ideia </p>
@@ -70,8 +105,4 @@ export default function Home() {
               width={500}
               height={500}
           />
-        </div>
-      </div>
-    </>
-  )
-}
+        </div> */}
